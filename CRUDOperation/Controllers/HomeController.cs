f6350042cbc338a31f3129b920bd9596b49cfe41
anilbhandari <boy.mistrey@gmail.com>
2017-02-17 19:38:51 +0545

    Add project files.

---
+++ CRUDOperation/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CRUDOperation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDOperation.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployees()
+        {
+
+            using (MyDatabaseEntities dc= new MyDatabaseEntities())
+            {
+            var employees=    dc.Employees.OrderBy(a => a.FirstName).ToList();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+
+
+            }
+           // return View();
+        }
+    }
+}
